0b42a6707d18282424783c174c78d63879ba4261
notrealFlakeyy <svenslucas@icloud.com>
2023-03-10 09:47:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeColor() {
+    let red = document.getElementById("red").value;
+    let green = document.getElementById("green").value;
+    let blue = document.getElementById("blue").value;
+    document.body.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+}
